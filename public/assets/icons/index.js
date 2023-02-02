cf3a22d6426640c9c93d48b9b9a7215ed6b5a2cc
misschievous <chiomaotu22@gmail.com>
2023-02-02 10:50:36 +0100

    added some icons

---
+++ public/assets/icons/index.js
@@ -0,0 +1,17 @@
+import closeBlack from './closeBlack.svg';
+import logo from './logo.svg';
+import hamburger from './hamburger.svg';
+import twitterIcon from './twitter.svg';
+import linkedinIcon from './linkedin.svg';
+import githubIcon from './github.svg';
+import messageIcon from './message.svg';
+
+export {
+  closeBlack,
+  logo,
+  hamburger,
+  twitterIcon,
+  linkedinIcon,
+  githubIcon,
+  messageIcon,
+};
