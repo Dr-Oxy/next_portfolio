ce0860b7fc1a5fef2270578fb46da710f37e5605
misschievous <chiomaotu22@gmail.com>
2023-02-02 10:49:39 +0100

    added custom color classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        milk: '#E2DDAC',
+        darkGray: '#515252',
+        lightGray: '#D9D9D9',
+        darkRed: '#2C0000',
+        brandYellow: '#E5B500',
+      },
+      dropShadow: {
+        '3xl': '0px 4px 4px rgba(0, 0, 0, 0.25)',
+        '4xl': [
+          '0 35px 35px rgba(0, 0, 0, 0.25)',
+          '0 45px 65px rgba(0, 0, 0, 0.15)',
+        ],
+      },
+      height: {
+        main: 'calc(100vh - 70px)',
+      },
+    },
   },
   plugins: [],
 };
